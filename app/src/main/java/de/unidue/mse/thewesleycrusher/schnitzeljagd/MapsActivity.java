962bc151ea90ca7f47b1495727c06ee1957371a8
Kathi <Katharina.Boese@stud.uni-due.de>
2017-06-28 16:55:08 +0200

    Merge branch 'master' of https://github.com/leifii/Schnitzeljagd
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/MapsActivity.java
+++ app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/MapsActivity.java
@@ -9,5 +9,11 @@ public class MapsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
+
+
+
+
     }
 }
