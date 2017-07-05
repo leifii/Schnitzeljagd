9dfa33782a21fab1ff31b04163664f10bd9309c3
Kathi <Katharina.Boese@stud.uni-due.de>
2017-07-05 14:02:01 +0200

    Merge branch 'master' of https://github.com/leifii/Schnitzeljagd
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/StartActivity.java
+++ app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/StartActivity.java
@@ -1,13 +1,44 @@
 package de.unidue.mse.thewesleycrusher.schnitzeljagd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+       Button start = (Button) findViewById(R.id.button_start);
+        start.setOnClickListener(this);
+       Button laden = (Button) findViewById(R.id.button_laden);
+        laden.setOnClickListener(this);
+       Button einstellungen = (Button) findViewById(R.id.button_einstellungen);
+        einstellungen.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.button_start:
+                startActivity(new Intent(StartActivity.this, MapsActivity.class));
+                break;
+
+            case R.id.button_laden:
+                startActivity(new Intent(StartActivity.this, MapsActivity.class));
+                break;
+
+            case R.id.button_einstellungen:
+                startActivity(new Intent(StartActivity.this, SettingsActivity.class));
+                break;
+        }
     }
 }
