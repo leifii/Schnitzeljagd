b47321d49e307607bcf57eb25c98bd9adb982175
MarkLeifeld <A>
2017-07-10 23:29:13 +0200

    Ha

--- app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/NewGameActivity.java
+++ app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/NewGameActivity.java
@@ -1,37 +1,37 @@
 package de.unidue.mse.thewesleycrusher.schnitzeljagd;
 
-import android.content.Intent;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class NewGameActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class NewGameActivity extends Activity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
-        Button foto =(Button) findViewById(R.id.button_setphoto);
+        Button foto = (Button) findViewById(R.id.button_setphoto);
         foto.setOnClickListener(this);
 
-        Button gps =(Button) findViewById(R.id.button_setgps);
+        Button gps = (Button) findViewById(R.id.button_setgps);
         gps.setOnClickListener(this);
 
-        Button naechster =(Button) findViewById(R.id.button_nextstop);
+        Button naechster = (Button) findViewById(R.id.button_nextstop);
         foto.setOnClickListener(this);
 
-        Button ende =(Button) findViewById(R.id.button_abschließen);
+        Button ende = (Button) findViewById(R.id.button_abschließen);
         foto.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
 
             case R.id.button_setphoto:
-                dispatchTakePictureIntent();
+
                 break;
             case R.id.button_setgps:
 
@@ -45,14 +45,7 @@ public class NewGameActivity extends AppCompatActivity implements View.OnClickLi
 
         }
     }
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
 }
+
+
+
