cadd6466fc68c3a824abf21429a316f6f893bc79
MarkLeifeld <A>
2017-07-12 21:19:07 +0200

    hi

--- app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/HinweisActivity.java
+++ app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/HinweisActivity.java
@@ -3,11 +3,11 @@ package de.unidue.mse.thewesleycrusher.schnitzeljagd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RestartActivity extends AppCompatActivity {
+public class HinweisActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restart);
+        setContentView(R.layout.activity_hinweis);
     }
 }
