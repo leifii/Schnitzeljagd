095d421fdba98827881dc6a8953d90575af07695
MarkLeifeld <A>
2017-07-12 15:56:56 +0200

    hi

--- app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/NewGameActivity.java
+++ app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/NewGameActivity.java
@@ -34,7 +34,6 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
