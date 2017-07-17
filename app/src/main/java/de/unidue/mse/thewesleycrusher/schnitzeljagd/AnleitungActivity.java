0e57b78dfea0eb46698c0b214b69c80e0a9a1372
Kathi <Katharina.Boese@stud.uni-due.de>
2017-07-17 23:29:58 +0200

    Merge branch 'master' of https://github.com/leifii/Schnitzeljagd
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/AnleitungActivity.java
+++ app/src/main/java/de/unidue/mse/thewesleycrusher/schnitzeljagd/AnleitungActivity.java
@@ -1,9 +1,16 @@
 package de.unidue.mse.thewesleycrusher.schnitzeljagd;
 
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.app.ActionBar;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class AnleitungActivity extends AppCompatActivity {
@@ -13,9 +20,31 @@ public class AnleitungActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anleitung);
 
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.rgb(32,192,4)));
 
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        super.onBackPressed();
+    }
+
 
 
 }
